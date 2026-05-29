939589542504989c901658225e7bcbf10332bb7b
JosephXZ1 <greninjamaster04@hotmail.com>
2026-05-28 22:40:04 -0500

    Otras cosas del Backend y luego la interfaz con componente y en App

--- Vue-Frontend/src/main.js
+++ Vue-Frontend/src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import axios from 'axios'
 
-const app = createApp(App)
+// Configuración global de la URL de Laravel
+window.axios = axios
+window.axios.defaults.baseURL = 'http://127.0.0.1:8000'
 
-app.use(router)
-
-app.mount('#app')
+createApp(App).mount('#app')
